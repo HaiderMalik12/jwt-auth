4f33ba08968c4426c5ea25622c95609e93ffb8de
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-26 21:20:26 +0500

    setup socket io library on backend

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,36 @@ import express from "express";
 import morgan from "morgan";
 import errorhandler from "error-handler";
 import cors from "cors";
+import http from "http";
+import socketIo, { Server as SocketIOServer, Socket } from "socket.io";
 
 import router from "./router";
 
 const app = express();
+
+/**
+ * Socket IO SETUP
+ */
+const server: http.Server = http.createServer(app);
+const io: SocketIOServer = new SocketIOServer(server);
+
+io.on("connection", (socket: Socket) => {
+  console.log("A user connected");
+
+  // Send existing products to the client upon connection
+  // socket.emit('products', products);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  // Listen for 'addProduct' event from the client
+  socket.on("addProduct", (newProduct) => {
+    // products.push(newProduct);
+    // Broadcast the new product to all connected clients
+    io.emit("productAdded", newProduct);
+  });
+});
 
 app.use(morgan("dev"));
 app.use(express.json());
