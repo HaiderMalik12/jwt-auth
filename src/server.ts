fb8d7c553bbf1fb3d60a86330672e9b7b34dbebf
HaiderMalik12 <haidermalik504@gmail.com>
2024-02-28 22:24:18 +0500

    removed comments

--- src/server.ts
+++ src/server.ts
@@ -19,14 +19,7 @@ const io = new Server(server, {
  * Socket IO SETUP
  */
 io.on("connection", (socket) => {
-  console.log("a user connected here");
-
   socket.on("product:add", (data) => {
-    // broadcast the allProducts event to all users/clients
-    console.log("product:add event called");
-    console.log(data);
-
-    // send product to all connected clients
     io.emit("product:created", data);
   });
 });
